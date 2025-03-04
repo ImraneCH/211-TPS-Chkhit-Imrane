389f4853020e20332330230e12ae652a53bb2b37
imraneCH <2324424@collegemv.qc.ca>
2025-03-04 13:35:08 -0500

    premier commit

---
+++ projet -react/src/Donnee.js
@@ -0,0 +1,34 @@
+export const profile = {
+    Prenom : "Jane", Nom : "Doe",
+    image : "https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces",
+    bio : "Lorem ipsum dolor sit, amet consectetur adipisicing elit 📷✈️🏕️",
+    profileStats : {
+        posts : 4,
+        followers : 188,
+        following : 206
+    }
+}
+
+export const posts = [
+    {
+        image : "https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/beautiful-game-500.jpg",
+        titre : "Psychopomp",
+        description : "Japanese Breakfast"
+    },
+    {
+        image : "https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-500.jpg",
+        titre : "let's go",
+        descrption : "In Love With A Ghost"
+    },
+  
+    {
+        image : "https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/lets-go-500.jpg",
+        titre : "The Beautiful Game",
+        descrption : "Vulfpeck"
+    },
+    {
+        image : "https://s3-us-west-2.amazonaws.com/s.cdpn.io/310408/jane-doe-500.jpg",
+        titre : "Jane Doe",
+        description : "Converge"
+    }
+]
