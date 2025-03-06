7258a2c9f0e14ddb4f460909ff80c6f1d16767b4
imraneCH <2324424@collegemv.qc.ca>
2025-03-05 21:02:47 -0500

    r

--- projet -react/src/Donnee.js
+++ projet -react/src/Donnee.js
@@ -3,7 +3,7 @@ export const profile = {
     image : "https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces",
     bio : "Lorem ipsum dolor sit, amet consectetur adipisicing elit 📷✈️🏕️",
     profileStats : {
-        posts : 4,
+        posts : 4 ,
         followers : 188,
         following : 206
     }
